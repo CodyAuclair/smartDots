cf5e4480bc1c1844fe4ef23dc5683f4645fbf46a
Cody <cody.auclair@charter.net>
2021-06-17 03:33:39 -0700

    Updated Gradle. Ran linter.

--- app/src/main/java/com/smartDots/SmartDots.java
+++ app/src/main/java/com/smartDots/SmartDots.java
@@ -1,10 +1,7 @@
 package com.smartDots;
 
 import android.app.Activity;
-import android.graphics.Point;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Display;
 
 public class SmartDots extends Activity {
 
